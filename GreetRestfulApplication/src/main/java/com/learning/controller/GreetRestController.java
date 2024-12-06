a85a64d743a8dd7fd956ff0c4ad8d5b89b8fa40e
dslraja <dslraja@yahoo.com>
2024-12-06 18:57:40 +0530

    Updated complete microservices application with ServiceRegistry ,AdminServer ,Feign client ,Api GateWay ,with Greet API & Welcome API commnication .

--- GreetRestfulApplication/src/main/java/com/learning/controller/GreetRestController.java
+++ GreetRestfulApplication/src/main/java/com/learning/controller/GreetRestController.java
@@ -2,20 +2,27 @@ package com.learning.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetRestController {
 
+	// environment property
+	@Autowired
+	private Environment environment;
+
 	@GetMapping("/greet")
 	public String greet() {
+		String port = environment.getProperty("server.port");
 		if (LocalDateTime.now().getHour() <= 12) {
-			return "Good Morning!";
+			return "Good Morning!" + "(" + port + ")";
 		} else if (LocalDateTime.now().getHour() >= 12) {
-			return "Good Afternoon!!";
+			return "Good Afternoon!!" + "(" + port + ")";
 		} else {
-			return "Good Night Sweet Dreams!!!";
+			return "Good Night Sweet Dreams!!!" + "(" + port + ")";
 		}
 	}
 
--- WelcomeRestfulApplication/src/main/java/com/learning/controller/WelcomeController.java
+++ WelcomeRestfulApplication/src/main/java/com/learning/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.learning.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,13 @@ public class WelcomeController {
 	@Autowired
 	private GreetFiegnClientInterface clientInterface;
 
+	@Autowired
+	private Environment environement;
+
 	@GetMapping("/welcome")
 	public String getWelcomeMsg() {
-		String welcomeMsg = "Welcome to My Universe!";
+		String port = environement.getProperty("server.port");
+		String welcomeMsg = "Welcome to My Universe! (" + port + ")";
 		String greet = clientInterface.invokeGreetApi();
 		return welcomeMsg + " ," + greet;
 	}
